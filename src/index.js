6f86964be77c3923502c09ef7e61b8e644b26fbb
Maryna Berezhanska <arina.lex2012@gmail.com>
2021-05-02 21:43:07 +0300

    menu-item.hbs

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const Theme = {
     DARK: 'dark-theme',
   };
 
-
 const switchElem = document.getElementById('theme-switch-toggle');
 switchElem.addEventListener('change', onChange);
 
@@ -15,17 +14,21 @@ if(localStorage.theme === Theme.DARK){
 };
 
 function onChange(event){ 
-     if (event.target.checked === true) { // если тема темная        
+    if (event.target.checked === true) {   // если тема темная        
         document.body.classList.add(Theme.DARK);
         document.body.classList.remove(Theme.LIGHT);
         localStorage.setItem('theme', Theme.DARK);
-        console.log(localStorage.theme);
     } else {
         document.body.classList.remove(Theme.DARK);
         document.body.classList.add(Theme.LIGHT);
         localStorage.setItem('theme', Theme.LIGHT);
     }; 
 };
+
+
+
+
+
 
 
 
